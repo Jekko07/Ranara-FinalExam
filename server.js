1d8d6ad5345b8f03b97157a6cccc08349b427f0a
Jekko07 <jerichojamesranara@gmail.com>
2017-03-20 09:50:14 -0700

    my first commit

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.get('/', function(req, res){
 app.get('*', function(req, res){
   res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
 });
+app.get('/about', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/about.html'));
+});
 //express server listen
 var server = app.listen(app.get('port'), function(){
   console.log('Server listening on port ',app.get('port'));
